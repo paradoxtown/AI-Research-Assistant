52267a305ce3e6f06cd276139bfce2efe6b34184
paradoxtown <parahyacinth@gmail.com>
2023-08-15 16:12:20 +0100

    fix a parameter in duckduck go search

--- actions/duck_search.py
+++ actions/duck_search.py
@@ -4,7 +4,7 @@ from duckduckgo_search import DDGS
 def duckduckgo_search(query, max_search_result=3):
     with DDGS() as ddgs:
         responses = list()
-        for i, r in enumerate(ddgs.text(query, region='wt-wt', safesearch='Off', timelimit='y')):
+        for i, r in enumerate(ddgs.text(query, region='wt-wt', safesearch='off', timelimit='y')):
             if i == max_search_result:
                 break
             responses.append(r)
